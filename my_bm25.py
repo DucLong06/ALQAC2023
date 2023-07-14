fcbebd3f747cef83aa09a08ccfeada90b4511662
DucLong06 <hoangduclongg@gmail.com>
2023-07-15 01:24:39 +0900

    update bm25 78->84

--- my_bm25.py
+++ my_bm25.py
@@ -11,19 +11,19 @@ from tqdm import tqdm
 import my_env
 
 
-PATH_TO_CORPUS = my_env.PATH_TO_CORPUS
+PATH_TO_CORPUS_ALL = "/Users/longhoangduc/Library/CloudStorage/GoogleDrive-hoangduclongg@gmail.com/My Drive/Colab Notebooks/Task1/data/raw/V1.1/law.json"
 PATH_TO_PUBLIC_TRAIN = my_env.PATH_TO_PUBLIC_TRAIN
 PATH_TO_PUBLIC_TEST = my_env.PATH_TO_PUBLIC_TEST
 
-corpus_df = df_create_corpus(PATH_TO_CORPUS)
+corpus_df = df_create_corpus(PATH_TO_CORPUS_ALL)
 id_corpus = corpus_df["id"].values.flatten().tolist()
 corpus = corpus_df["article"].values.flatten().tolist()
 
 
-tokenized_corpus = [word_segment(doc) for doc in corpus]
-tokenized_corpus = [doc.split(" ") for doc in tokenized_corpus]
+# tokenized_corpus = [word_segment(doc) for doc in corpus]
+tokenized_corpus = [doc.split(" ") for doc in corpus]
 
-bm25 = BM25Plus(tokenized_corpus)
+bm25 = BM25Okapi(tokenized_corpus)
 with open(os.path.join(my_env.PATH_TO_SAVE_MODEL, "my_bm25.pkl"), "wb") as bm_file:
     pickle.dump(bm25, bm_file)
 
@@ -32,7 +32,7 @@ def test():
     test_df = df_create_questions_test(PATH_TO_PUBLIC_TRAIN)
     querys = test_df["question"].values.flatten().tolist()
     relevants = test_df["relevant_id"].values.flatten().tolist()
-    tokenized_querys = [word_segment(query) for query in querys]
+    # tokenized_querys = [word_segment(query) for query in querys]
 
     correct_predictions = 0
     total_questions = len(querys)
@@ -40,7 +40,7 @@ def test():
     false_positive = 0
     false_negative = 0
 
-    for tokenized_query, relevant in tqdm(zip(tokenized_querys, relevants)):
+    for tokenized_query, relevant in tqdm(zip(querys, relevants)):
         tokenized_query = tokenized_query.split(" ")
         similarity_scores = bm25.get_scores(tokenized_query)
         max_similarity_index = np.argmax(similarity_scores)
@@ -86,6 +86,5 @@ def test_submit(path_json: str):
         json.dump(out_put, f, ensure_ascii=False)
 
 
-
-# test()
+test()
 # test_submit(PATH_TO_PUBLIC_TEST)
