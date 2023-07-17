5a7a31478b6341cfe9b174c956e8fa6840da63cc
DucLong06 <hoangduclongg@gmail.com>
2023-07-17 22:55:41 +0900

    update bot tele

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,25 +1,28 @@
 import datetime
 from telegram import Bot
-from tabulate import tabulate
+import my_env
 
 
-def send_telegram_message(model_name, model_parameter, data_name, alpha, top_k_bm25, accuracy, precision, recall, f2, note):
-    token, chat_id = """"""
-    bot = Bot(token=token)
-
+async def send_telegram_message(model_name="", model_parameter="", data_name="", alpha="", top_k_bm25="", accuracy="", precision="", recall="", f2="", note=""):
+    bot = Bot(token=my_env.TOKEN_BOT)
     current_time = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
 
-    headers = ["Date", "Model", "Model Parameter", "Data", "Alpha",
-               "Top K BM25", "Accuracy", "Precision", "Recall", "F2", "Note"]
-    data = [
-        [current_time, model_name, model_parameter, data_name, alpha, top_k_bm25,
-            f"{accuracy:.4f}", f"{precision:.4f}", f"{recall:.4f}", f"{f2:.4f}", note]
-    ]
-    table = tabulate(data, headers=headers, tablefmt="grid")
-
+    # Format the message as key-value pairs
     message = f"<b>Training Results:</b>\n\n"
-    message += f"{table}"
+    message += f"<b>Date:</b> {current_time}\n"
+    message += f"<b>Model:</b> {model_name}\n"
+    message += f"<b>Model Parameter:</b> {model_parameter}\n"
+    message += f"<b>Data:</b> {data_name}\n"
+    message += f"<b>Alpha:</b> {alpha}\n"
+    message += f"<b>Top K BM25:</b> {top_k_bm25}\n"
+    message += f"<b>Accuracy:</b> {accuracy}\n"
+    message += f"<b>Precision:</b> {precision}\n"
+    message += f"<b>Recall:</b> {recall}\n"
+    message += f"<b>F2:</b> {f2}\n"
+    message += f"<b>Note:</b> {note}"
 
-    bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
+    await bot.send_message(chat_id=my_env.ID_TELEGRAM,
+                           text=message, parse_mode='HTML')
 
     print("Sent Telegram message successfully.")
+
