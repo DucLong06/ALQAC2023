7a80ea38a3bcdc4e54175a245f83ae47819ed2db
DucLong06 <hoangduclongg@gmail.com>
2023-07-19 12:59:30 +0900

    config bottele

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,26 +1,27 @@
+import asyncio
 import datetime
 from telegram import Bot
 import my_env
-
+import html
 
 async def send_telegram_message(model_name="", model_parameter="", data_name="", alpha="", top_k_bm25="", accuracy="", precision="", recall="", f2="", note=""):
     bot = Bot(token=my_env.TOKEN_BOT)
     current_time = datetime.datetime.now().strftime('%d/%m/%Y %H:%M')
 
-    # Format the message as key-value pairs
-    message = f"Results:\n\n"
-    message += f"Date: {current_time}\n"
-    message += f"Model: {model_name}\n"
-    message += f"Model Parameter: {model_parameter}\n"
-    message += f"Data: {data_name}\n"
-    message += f"Alpha: {alpha}\n"
-    message += f"Top K BM25: {top_k_bm25}\n"
-    message += f"Accuracy: {accuracy}\n"
-    message += f"Precision: {precision}\n"
-    message += f"Recall: {recall}\n"
-    message += f"F2: {f2}\n"
-    message += f"Note: {note}"
+    message = f"<b>Results:</b>\n\n"
+    message += f"<b>Date:</b> {html.escape(current_time)}\n"
+    message += f"<b>Model:</b> {html.escape(model_name)}\n"
+    message += f"<b>Model Parameter:</b> {html.escape(model_parameter)}\n"
+    message += f"<b>Data:</b> {html.escape(data_name)}\n"
+    message += f"<b>Alpha:</b> {html.escape(alpha)}\n"
+    message += f"<b>Top K BM25:</b> {html.escape(top_k_bm25)}\n"
+    message += f"<b>Accuracy:</b> {html.escape(accuracy)}\n"
+    message += f"<b>Precision:</b> {html.escape(precision)}\n"
+    message += f"<b>Recall:</b> {html.escape(recall)}\n"
+    message += f"<b>F2:</b> {html.escape(f2)}\n"
+    message += f"<b>Note:</b> {html.escape(note)}"
 
-    await bot.send_message(chat_id=my_env.ID_TELEGRAM,text=message)
+    await bot.send_message(chat_id=my_env.ID_TELEGRAM, text=message, parse_mode='HTML')
 
     print("Sent Telegram message successfully.")
+
