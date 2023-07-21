0f065a81e0000f2a7436cc0b237f6361897e036b
DucLong06 <hoangduclongg@gmail.com>
2023-07-22 02:25:45 +0900

    add class early stop

--- train.py
+++ train.py
@@ -6,16 +6,19 @@ import pandas as pd
 import torch
 from torch.utils.data import DataLoader, random_split
 from bot_telegram import send_message, send_telegram_message
+from early_stopping import EarlyStopping
 from eval_metrics import eval_model
 from law_data import Law_Dataset
 from model_paraformer import Model_Paraformer
-from raw_data import df_create_data_training
+from raw_data import data_training_generator
 import my_env
 import asyncio
 from tqdm import tqdm
 import my_logger
+import evaluate
 
 logger = my_logger.Logger("training", my_env.LOG)
+
 
 def preprocessor_batch(batch):
     questions, articles, relevants = zip(*batch)
@@ -27,52 +30,39 @@ def preprocessor_batch(batch):
 
     return questions, padded_articles, relevants
 
-def train_valid_test_split(dataset, train_ratio, valid_ratio, test_ratio):
-    assert train_ratio + valid_ratio + test_ratio == 1, "The ratios should add up to 1."
-    total_size = len(dataset)
-    train_size = int(train_ratio * total_size)
-    valid_size = int(valid_ratio * total_size)
-    test_size = total_size - train_size - valid_size
 
-    # Use random_split to split the dataset
-    train_dataset, valid_dataset, test_dataset = random_split(dataset, [train_size, valid_size, test_size])
-
-    return train_dataset, valid_dataset, test_dataset
-
-def generate_model_name(original_string):
+def generate_model_name(original_string, path_to_save=my_env.PATH_TO_SAVE_MODEL):
     now = datetime.datetime.now()
     time_string = now.strftime("%Y%m%d%H%M%S")
     clean_time_string = re.sub(r'\W+', '', time_string)
-    original_string = original_string.replace("/","-")
-    return f"{original_string}_{clean_time_string}.pth"
+    original_string = original_string.replace("/", "-")
+    return os.path.join(path_to_save,
+                        f"{original_string}_{clean_time_string}.pth")
 
 
-def train(base_model:str="keepitreal/vietnamese-sbert"):
-    df_train = df_create_data_training(
-        my_env.PATH_TO_PUBLIC_TRAIN, my_env.PATH_TO_CORPUS_2023, top_bm25=20)
-    df_train = Law_Dataset(df_train)
-
-    # Split dataset into train and test set
-    train_ratio = 0.8
-    valid_ratio = 0.1
-    test_ratio = 0.1
-    train_dataset, valid_dataset, test_dataset = train_valid_test_split(df_train, train_ratio, valid_ratio, test_ratio)
+def train(base_model, input_questions, input_articles, top_bm25, batch_size, max_epochs):
+    train_df, val_df, test_df = data_training_generator(
+        input_questions, input_articles, top_bm25=top_bm25, train_ratio=0.8, val_ratio=0.1)
+    train_dataset = Law_Dataset(train_df)
+    valid_dataset = Law_Dataset(val_df)
+    test_dataset = Law_Dataset(test_df)
 
     train_dataloader = DataLoader(
-        train_dataset, batch_size=64, num_workers=4, shuffle=True, collate_fn=preprocessor_batch)
+        train_dataset, batch_size=batch_size, num_workers=4, shuffle=True, collate_fn=preprocessor_batch)
     valid_dataloader = DataLoader(
-        valid_dataset, batch_size=64, num_workers=4, shuffle=False, collate_fn=preprocessor_batch)
+        valid_dataset, batch_size=batch_size, num_workers=4, shuffle=True, collate_fn=preprocessor_batch)
     test_dataloader = DataLoader(
-        test_dataset, batch_size=64, num_workers=4, shuffle=False,collate_fn=preprocessor_batch)
+        test_dataset, batch_size=batch_size, num_workers=4, shuffle=True, collate_fn=preprocessor_batch)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     model = Model_Paraformer(base_model).to(device)
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=3e-5, eps=1e-8)
+    optimizer = model.configure_optimizers()
 
-    max_epochs = 1
-    name_model = generate_model_name(base_model)
+    path_name_model = generate_model_name(base_model)
+    early_stopping = EarlyStopping(patience=3, verbose=True, delta=0.0001)
+
     # scheduler = lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
 
     for epoch in range(max_epochs):
@@ -92,12 +82,28 @@ def train(base_model:str="keepitreal/vietnamese-sbert"):
             optimizer.step()
             # scheduler.step(loss)
             total_loss += loss.item()
-            
-        _,accuracy, _, _, _= eval_model(valid_dataloader, model)
+
+            # Validation loss calculation without backpropagation
+        model.eval()
+        val_total_loss = 0.0
+        with torch.no_grad():
+            for val_batch in tqdm(valid_dataloader):
+                val_questions, val_articles, val_relevants = val_batch
+                val_logits = model.forward(val_questions, val_articles)
+                val_relevants = torch.tensor(val_relevants).to(device)
+                val_loss = model.criterion(val_logits, val_relevants)
+                val_total_loss += val_loss.item()
+
+        avg_val_loss = val_total_loss / len(valid_dataloader)
         train_loss = total_loss / len(train_dataloader)
-       
-        logger.info(f"Validation acc: {accuracy:.4f}")
+
+        logger.info(f"Validation Loss: {avg_val_loss:.4f}")
         logger.info(f"Train Loss: {train_loss:.4f}")
+
+        early_stopping(avg_val_loss, model)
+        if early_stopping.early_stop:
+            logger.info("Early stopping")
+            break
         logger.info("================================================")
 
     logger.info('Training finished.')
@@ -106,14 +112,14 @@ def train(base_model:str="keepitreal/vietnamese-sbert"):
 
     _, test_accuracy, test_precision, test_recall, test_f2_score = eval_model(
         test_dataloader, model)
-   
+
     try:
         asyncio.run(send_telegram_message(
-            model_name="[TRAIN] Paraformer",
+            model_name=f"[TRAIN]{path_name_model}",
             model_base=f"base_model: {base_model}",
-            data_name="train 2023",
+            data_name=f"question:{input_questions} articles:{input_articles}",
             alpha="none",
-            top_k_bm25="20",
+            top_k_bm25=top_bm25,
             accuracy=test_accuracy,
             precision=test_precision,
             recall=test_recall,
@@ -129,19 +135,28 @@ def train(base_model:str="keepitreal/vietnamese-sbert"):
     logger.info(f"Test F2 Score: {test_f2_score:.4f}")
 
     # Save the model
-    torch.save(model.state_dict(), os.path.join(my_env.PATH_TO_SAVE_MODEL, name_model))
-    logger.info("Done !!!")
+    torch.save(model.state_dict(), path_name_model)
+    logger.info("Done Training !!!")
 
 
 if __name__ == "__main__":
-    # for base_model in my_env.list_base_model:
-    #     logger.info(f"Training: {base_model}")
-    #     try:
-    #         train(base_model)
-    #         logger.info(f"Done: {base_model}")
-    #     except Exception as e:
-    #         logger.error(f"{base_model}:{str(e)}")
-    #         asyncio.run(send_message(f"base model error : {base_model}"))
-    train()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--base_model', type=str,
+                        default="keepitreal/vietnamese-sbert",
+                        help="Base model to use for Paraformer.")
+    parser.add_argument('--input_questions', type=str,
+                        default=my_env.PATH_TO_PUBLIC_TRAIN,
+                        help="Path to the input questions data file.")
+    parser.add_argument('--input_articles', type=str,
+                        default=my_env.PATH_TO_CORPUS_2023,
+                        help="Path to the input articles data file.")
+    parser.add_argument('--top_bm25', type=int, default=10,
+                        help="Number of top fake BM25 articles to consider.")
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help="Batch size for training.")
+    parser.add_argument('--max_epochs', type=int, default=20,
+                        help="Maximum number of epochs for training.")
+    args = parser.parse_args()
 
- 
+    train(args.base_model, args.input_questions, args.input_articles,
+          args.top_bm25, args.batch_size, args.max_epochs)
